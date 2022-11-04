cf1f33b18f1a80e1aeee26994bf3f0b2983dd3c7
Joao S. Martins <john.martins@sbcglobal.net>
2022-11-03 23:47:51 -0700

    Sabbatical page.

---
+++ docs/photos/events/2022/sabbatical/img/index.tsx
@@ -0,0 +1,44 @@
+import IMG01 from './img-1.jpg';
+import IMG02 from './img-2.jpg';
+import IMG03 from './img-3.jpg';
+import IMG04 from './img-4.jpg';
+import IMG05 from './img-5.jpg';
+import IMG06 from './img-6.jpg';
+import IMG07 from './img-7.jpg';
+import IMG08 from './img-8.jpg';
+import IMG09 from './img-9.jpg';
+import IMG10 from './img-10.jpg';
+import IMG11 from './img-11.jpg';
+import IMG12 from './img-12.jpg';
+import IMG13 from './img-13.jpg';
+import IMG14 from './img-14.jpg';
+import IMG15 from './img-15.jpg';
+import IMG16 from './img-16.jpg';
+import IMG17 from './img-17.jpg';
+import IMG18 from './img-18.jpg';
+import IMG19 from './img-19.jpg';
+import IMG20 from './img-20.jpg';
+import IMG21 from './img-21.jpg';
+import IMG22 from './img-22.jpg';
+import IMG23 from './img-23.jpg';
+import IMG25 from './img-25.jpg';
+import IMG26 from './img-26.jpg';
+import IMG27 from './img-27.jpg';
+import IMG28 from './img-28.jpg';
+import IMG29 from './img-29.jpg';
+import IMG30 from './img-30.jpg';
+import IMG31 from './img-31.jpg';
+import IMG32 from './img-32.jpg';
+import IMG33 from './img-33.jpg';
+import IMG35 from './img-35.jpg';
+import IMG36 from './img-36.jpg';
+import IMG37 from './img-37.jpg';
+import IMG38 from './img-38.jpg';
+import IMG39 from './img-39.jpg';
+
+export default [
+  IMG01, IMG02, IMG03, IMG04, IMG05, IMG06, IMG07, IMG08, IMG09, IMG10,
+  IMG11, IMG12, IMG13, IMG14, IMG15, IMG16, IMG17, IMG18, IMG19, IMG20,
+  IMG21, IMG22, IMG23, null, IMG25, IMG26, IMG27, IMG28, IMG29, IMG30,
+  IMG31, IMG32, IMG33, null, IMG35, IMG36, IMG37, IMG38, IMG39
+];
---
+++ docs/photos/events/2022/sabbatical/vid/index.tsx
@@ -0,0 +1,10 @@
+import VID01 from './vid-1.webm';
+import VID02 from './vid-2.webm';
+import VID03 from './vid-3.webm';
+import VID04 from './vid-4.webm';
+import VID05 from './vid-5.webm';
+import VID06 from './vid-6.webm';
+
+export default [
+  VID01, VID02, VID03, VID04, VID05, VID06
+];

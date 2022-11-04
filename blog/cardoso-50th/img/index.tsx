0ac208e7b67fdcd32a0128ec223b66a8806ade69
Joao S. Martins <john.martins@sbcglobal.net>
2022-11-04 15:51:34 -0700

    Move photo stories to blog posts.

---
+++ blog/cardoso-50th/img/index.tsx
@@ -0,0 +1,23 @@
+import IMG01 from './img-1.jpg';
+import IMG02 from './img-2.jpg';
+import IMG03 from './img-3.jpg';
+import IMG04 from './img-4.jpg';
+import IMG05 from './img-5.jpg';
+import IMG06 from './img-6.jpg';
+import IMG07 from './img-7.jpg';
+import IMG08 from './img-8.jpg';
+import IMG09 from './img-9.jpg';
+import IMG10 from './img-10.jpg';
+import IMG11 from './img-11.jpg';
+import IMG12 from './img-12.jpg';
+import IMG13 from './img-13.jpg';
+import IMG14 from './img-14.jpg';
+import IMG15 from './img-15.jpg';
+import IMG16 from './img-16.jpg';
+import IMG17 from './img-17.jpg';
+import IMG18 from './img-18.jpg';
+
+export default [
+  IMG01, IMG02, IMG03, IMG04, IMG05, IMG06, IMG07, IMG08, IMG09, IMG10,
+  IMG11, IMG12, IMG13, IMG14, IMG15, IMG16, IMG17, IMG18, 
+];

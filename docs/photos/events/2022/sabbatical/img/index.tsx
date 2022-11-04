0637678bcb49de54f36403a97cfb55c0178b9b81
Joao S. Martins <john.martins@sbcglobal.net>
2022-11-04 10:03:19 -0700

    Updates to the sabbatical page and figcaption header styles.

--- docs/photos/events/2022/sabbatical/img/index.tsx
+++ docs/photos/events/2022/sabbatical/img/index.tsx
@@ -3,17 +3,14 @@ import IMG02 from './img-2.jpg';
 import IMG03 from './img-3.jpg';
 import IMG04 from './img-4.jpg';
 import IMG05 from './img-5.jpg';
-import IMG06 from './img-6.jpg';
 import IMG07 from './img-7.jpg';
 import IMG08 from './img-8.jpg';
 import IMG09 from './img-9.jpg';
 import IMG10 from './img-10.jpg';
 import IMG11 from './img-11.jpg';
-import IMG12 from './img-12.jpg';
 import IMG13 from './img-13.jpg';
 import IMG14 from './img-14.jpg';
 import IMG15 from './img-15.jpg';
-import IMG16 from './img-16.jpg';
 import IMG17 from './img-17.jpg';
 import IMG18 from './img-18.jpg';
 import IMG19 from './img-19.jpg';
@@ -37,8 +34,8 @@ import IMG38 from './img-38.jpg';
 import IMG39 from './img-39.jpg';
 
 export default [
-  IMG01, IMG02, IMG03, IMG04, IMG05, IMG06, IMG07, IMG08, IMG09, IMG10,
-  IMG11, IMG12, IMG13, IMG14, IMG15, IMG16, IMG17, IMG18, IMG19, IMG20,
+  IMG01, IMG02, IMG03, IMG04, IMG05, null, IMG07, IMG08, IMG09, IMG10,
+  IMG11, null, IMG13, IMG14, IMG15, null, IMG17, IMG18, IMG19, IMG20,
   IMG21, IMG22, IMG23, null, IMG25, IMG26, IMG27, IMG28, IMG29, IMG30,
   IMG31, IMG32, IMG33, null, IMG35, IMG36, IMG37, IMG38, IMG39
 ];

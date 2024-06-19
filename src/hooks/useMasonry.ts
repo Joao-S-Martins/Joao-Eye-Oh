90b5ede015d532f14df743b03f5adcdcdb6066e7
Joao S. Martins <3526607+Joao-S-Martins@users.noreply.github.com>
2024-06-19 11:56:44 -0700

    Fix build for Masonry import

--- src/hooks/useMasonry.ts
+++ src/hooks/useMasonry.ts
@@ -1,8 +1,10 @@
 import { useEffect, RefObject } from 'react';
-import Masonry from 'masonry-layout';
+import { Options as MasonryOptions } from 'masonry-layout';
+// import Masonry from 'masonry-layout'; // Causes build failure because it requires window object
 
-function useMasonry(masonRef: RefObject<HTMLElement>, masonryOpts: Masonry.Options) {
+function useMasonry(masonRef: RefObject<HTMLElement>, masonryOpts: MasonryOptions) {
   useEffect(() => {
+    const Masonry = require('masonry-layout');
     const mason = new Masonry(masonRef.current, masonryOpts);
     const resizeObserver = new ResizeObserver(() => {mason.layout()});
     resizeObserver.observe(masonRef?.current.parentElement);
